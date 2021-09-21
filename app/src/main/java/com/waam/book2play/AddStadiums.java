f651deaad966e52482341ad628ae38e9e7b5433a
Asel <asel.jaya@gmail.com>
2021-09-22 00:50:02 +0530

    Insert stadium image added

--- app/src/main/java/com/waam/book2play/AddStadiums.java
+++ app/src/main/java/com/waam/book2play/AddStadiums.java
@@ -1,18 +1,32 @@
 package com.waam.book2play;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.net.URI;
+
 public class AddStadiums extends AppCompatActivity {
 
     private TextInputLayout sPhoneLayout, sNameLayout, sAddressLayout, sOTLayout, sCTLayout, sPriceLayout;
     private TextInputEditText sPhone, sName, sAddress, sOT, sCT, sPrice;
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private Button sChooseImageButton;
+    private Button sRegisterButton;
+    private ImageView sImageView;
+    private ProgressBar sUploadProgress;
+    private Uri imageURI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +45,17 @@ public class AddStadiums extends AppCompatActivity {
         sOTLayout = findViewById(R.id.openTimeLayout);
         sCTLayout = findViewById(R.id.closeTimeLayout);
         sPriceLayout = findViewById(R.id.stadiumPriceLayout);
+        sChooseImageButton = findViewById(R.id.btn_addImage);
+        sRegisterButton = findViewById(R.id.btn_stadiumRegister);
+        sImageView = findViewById(R.id.iv_sUploadImage);
+        sUploadProgress = findViewById(R.id.progressBar);
+
+        sChooseImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openImageChooser();
+            }
+        });
     }
 
     private boolean validatePhone(){
@@ -50,7 +75,7 @@ public class AddStadiums extends AppCompatActivity {
     private boolean validateStadiumName(){
         String sname = sName.getText().toString().trim();
         if (sname.isEmpty()){
-            sName.setError("Field Cannot be empty.");
+            sNameLayout.setError("Field Cannot be empty.");
             return false;
         }else{
             sName.setError(null);
@@ -122,4 +147,20 @@ public class AddStadiums extends AppCompatActivity {
         Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
     }
 
+    private void openImageChooser(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null){
+            imageURI = data.getData();
+            sImageView.setImageURI(imageURI);
+        }
+    }
 }
