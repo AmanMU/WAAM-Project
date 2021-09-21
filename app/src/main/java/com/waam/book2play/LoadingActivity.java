23d73d589475e8747dd18904a6c73d5a0c974ca9
AmanMU <IT20155520@my.sliit.lk>
2021-09-21 17:50:45 +0530

    updated onbackpressed methods

--- app/src/main/java/com/waam/book2play/LoadingActivity.java
+++ app/src/main/java/com/waam/book2play/LoadingActivity.java
@@ -1,23 +1,20 @@
 package com.waam.book2play;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import java.util.concurrent.TimeUnit;
 
 public class LoadingActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     FirebaseAuth.AuthStateListener mAuthListener;
     ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,6 +46,7 @@ public class LoadingActivity extends AppCompatActivity {
         super.onResume();
         mAuth.addAuthStateListener(mAuthListener);
     }
+
     @Override
     protected void onStop() {
         super.onStop();
