2d7241a01a7fb9686283b2a39e7fea2ab193a018
Asel <asel.jaya@gmail.com>
2021-09-22 19:12:03 +0530

    button functions of stadium owner home

--- app/src/main/java/com/waam/book2play/StadiumOwnerHome.java
+++ app/src/main/java/com/waam/book2play/StadiumOwnerHome.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
@@ -42,7 +43,22 @@ public class StadiumOwnerHome extends AppCompatActivity implements NavigationVie
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
+        switch (item.getItemId()){
+            case R.id.nav_ownerHome:
+                getSupportFragmentManager().beginTransaction().replace(R.id.oFragment_container, new Home()).commit();
+                break;
+            case R.id.nav_myStadiums:
+                getSupportFragmentManager().beginTransaction().replace(R.id.oFragment_container, new MyStadiums()).commit();
+                break;
+            case R.id.nav_addStadium:
+                //getSupportFragmentManager().beginTransaction().replace(R.id.oFragment_container, new MyStadiums()).commit();
+                break;
+            case R.id.nav_ownerLogout:
+                //mAuth.signOut();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
